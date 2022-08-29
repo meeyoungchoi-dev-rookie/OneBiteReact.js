d19ea19d882b74b7767b16dff59904ef43d1c511
meeyoungchoi <goodlife1359@gmail.com>
2022-08-29 22:07:32 +0900

    헤더와 푸터를 컴포넌트로 분리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+
 
 function App() {
   let name = "묭이";
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
        <h2>안녕 리액트 {name}</h2>
       </header>
+      <MyFooter/>
     </div>
   );
 }
