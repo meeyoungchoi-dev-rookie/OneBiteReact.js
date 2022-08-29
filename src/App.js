e392af8b6b5488a6e0792e7da91b4e036ab4ff9f
meeyoungchoi <goodlife1359@gmail.com>
2022-08-29 22:16:28 +0900

    css 파일을 import 하지 않고 html 요소에 스타일 입히기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
 
 
 function App() {
   let name = "묭이";
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
+
+
   return (
-    <div className="App">
+    <div className="App" style={stype.App}>
       <MyHeader />
       
-       <h2>안녕 리액트 {name}</h2>
-       <b id="bold_text">React.js</b> 
+       <h2 style={style.h2}>안녕 리액트 {name}</h2>
+       <b id="bold_text" style={style.bold_text}>React.js</b> 
 
       <MyFooter/>
     </div>
